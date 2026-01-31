a2f0f956866cfaaa196fc94e87ba06dc4983db22
yamato3010 <yama3to10yama3@gmail.com>
2026-01-31 12:16:56 +0900

    fix: 🐛 横画面にならないよう修正

--- yorunote/yorunoteApp.swift
+++ yorunote/yorunoteApp.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 import SwiftData
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
 
 @main
 struct yorunoteApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             NightEntry.self,
@@ -26,6 +36,10 @@ struct yorunoteApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    // 縦向き固定
+                    AppDelegate.orientationLock = UIInterfaceOrientationMask.portrait
+                }
         }
         .modelContainer(sharedModelContainer)
     }
